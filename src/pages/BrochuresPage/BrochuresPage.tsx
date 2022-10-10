7a3d44cef2fb428817e0bb074828163a9a04df05
Martin Li <martinhappy2017@icloud.com>
2022-10-10 20:03:01 +0300

    add button, add brochure counter

--- src/pages/BrochuresPage/BrochuresPage.tsx
+++ src/pages/BrochuresPage/BrochuresPage.tsx
@@ -1,13 +1,18 @@
+import { Button } from 'components/Button';
 import { Container } from 'components/Container';
 import { Modal } from 'components/Modal'
 import { FC, useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from 'shared/hooks/redux';
 import { toggleBrochure } from 'store/slices/brochures/brochures';
+import { getSelectedBrochuresIds } from 'store/slices/brochures/selector';
 import { getBrochuresThunk } from 'store/slices/brochures/thunk';
 import { BrochureCard } from './BrochureCard';
 import s from './BrochuresPage.module.scss';
+
 export const BrochuresPage: FC = () => {
     const brochures = useAppSelector((state) => state.brochures.brochures)
+    const totalSelected = useAppSelector(getSelectedBrochuresIds);
+    const totalCounter = totalSelected.length;
     const dispatch = useAppDispatch();
 
     useEffect(() => {
@@ -20,7 +25,7 @@ export const BrochuresPage: FC = () => {
                 <div className={s.title}>Brochures</div>
                 <div className={s.nav}>
                     <div className={s.subtitle} >Faucibus enim sit leo, purus, odio erat. Neque scelerisque volutpat morbi proin. Massa quis montes, scelerisque commodo elit erat in urna id. Purus sit odio egestas venenatis viverra blandit amet vitae.</div>
-                    <div className={s.selectBtn}>Send me selected</div>
+                    {totalCounter ? <Button variant='secondary'>Send me selected ({totalCounter})</Button> : <div className={s.selectBtn}>Send me selected</div>}
                 </div>
                 <div className={s.sort}>Sort</div>
                 <div className={s.brochuresList}>
