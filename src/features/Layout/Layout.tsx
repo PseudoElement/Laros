de77c0362f658d9139caac9ff48eef148c222a27
Martin Li <martinhappy2017@icloud.com>
2022-10-17 13:45:47 +0300

    add react-tabs, add about layout

--- src/features/Layout/Layout.tsx
+++ src/features/Layout/Layout.tsx
@@ -4,11 +4,16 @@ import { FC, ReactNode } from 'react'
 import { useCollapsedHeader } from 'shared/hooks/useCollapsedHeader';
 import s from './Layout.module.scss'
 import cn from 'classnames';
+import { useRouter } from 'next/router';
+import { AboutLayout } from 'features/AboutLayout';
+import { getAboutTabIndex } from 'shared/helpers/layout';
 interface LayoutProps {
     children: ReactNode;
 };
 
 export const Layout: FC<LayoutProps> = ({ children }) => {
+    const { pathname } = useRouter()
+    const aboutPage = pathname.includes('/about')
     const isCollapsed = useCollapsedHeader();
     const containerClass = cn(s.container, {
         [s.collapsed]: isCollapsed
@@ -19,7 +24,7 @@ export const Layout: FC<LayoutProps> = ({ children }) => {
             <Header />
             <main className={containerClass}>
                 <div className={s.content}>
-                    {children}
+                    {aboutPage ? <AboutLayout tab={getAboutTabIndex(pathname)}>{children}</AboutLayout> : children}
                 </div>
             </main>
             <Footer />
