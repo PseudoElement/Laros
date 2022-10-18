c6ab5b9bdc11b1d36bd97ddd7dac120bde99934b
Toma <ragimova-tamara@mail.ru>
2022-10-18 18:40:13 +0400

    some refactor

--- src/pages/CareerPage/JobCard/JobCard.tsx
+++ src/pages/CareerPage/JobCard/JobCard.tsx
@@ -1,24 +1,26 @@
+import { Button } from 'components/Button'
 import { FC } from 'react'
 import s from './JobCard.module.scss'
 
-export type jobCard = {
-  vacancy: string,
-  time: string,
-  description: string,
-  id?: number
+export interface jobCardProps {
+  vacancy: string
+  time: string
+  description: string
+  id: number
 }
 
-
-export const JobCard: FC<jobCard> = ({vacancy, time, description} ) => {
-    return (
-       <div className={s.cardWrapper}>
-         <h2 className={s.title}>{vacancy}</h2>
-         <div className={s.time}>{time}</div>
-         <p className={s.description}>{description}</p>
-         <div className={s.buttons}>
-           <button className={s.applyButton}>Apply for a job</button>
-           <button className={s.infoButton}>More info</button>
-         </div>
-       </div>
-    )
+export const JobCard: FC<jobCardProps> = ({ vacancy, time, description }) => {
+  return (
+    <div className={s.cardWrapper}>
+      <h2 className={s.title}>{vacancy}</h2>
+      <div className={s.time}>{time}</div>
+      <p className={s.description}>{description}</p>
+      <div className={s.buttons}>
+        <Button classname={s.applyButton}>Apply for a job</Button>
+        <Button classname={s.infoButton} variant={'outline'}>
+          More info
+        </Button>
+      </div>
+    </div>
+  )
 }
