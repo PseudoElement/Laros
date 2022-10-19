e47169ede0966811cc141aa39d98b02c76ab7c6b
Magomed <rostovmen95@gmail.com>
2022-10-19 07:46:01 +0300

    i have fixed bags

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,23 +1,22 @@
 import { FC, ReactNode } from 'react'
-
-import s from './CheckBox.module.scss'
-
+import s from './Checkbox.module.scss'
 
 
-interface checkBox {
+
+interface СheckboxProps {
     onChange: () => void
     value: boolean
-    name: ReactNode
+    label: ReactNode
 }
 
-export const CheckBox: FC<checkBox> = ({onChange, value}) => {
-
+export const Checkbox: FC<СheckboxProps> = ({onChange, value}) => {
     return (
+    <div className={s.checkboxWrapper}>
         <label className={s.label}>
-            <input
-            className={s.check__input} type="checkbox"/>
-            <span onClick={onChange} className={value ? s.check__box__active : s.check__box}></span>
+            <input className={s.checkInput} type="checkbox"/>
+            <span onClick={onChange} className={value ? s.checkBoxActive : s.checkBox}></span>
         </label>
+    </div>
     );
 }
 
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,11 +3,14 @@ import { Modal } from 'components/Modal'
 import { Radio } from 'components/Radio'
 import { ContactForm } from 'features/ContactForm'
 import { FC, useState } from 'react'
+import {Checkbox} from "../../components/Checkbox";
 
 export const HomePage: FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [input, setInput] = useState<string | number>('')
   const [checkboxValue, setCheckboxValue] = useState('man')
+
+
   //TODO move to example page
   return <div style={{ marginTop: '15px', width: '400px', backgroundColor: '#FAFBFC', width: '100%' }}>
     <div style={{ marginTop: '15px' }} onClick={() => setIsModalOpen(true)} ></div>
@@ -16,6 +19,8 @@ export const HomePage: FC = () => {
     <Input id='num' value='' type='phone' label='Number' onChange={(v) => setInput(v)} />
     <Input placeholder='Mark' id='name' value={input} label='Fullname and surname' onChange={(v) => setInput(v)} />
     <Modal title='Contact Form' isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>Hello!</Modal>
+
+    <Checkbox onChange={() =>{}} value={true}/>
   </div>
 
 }
