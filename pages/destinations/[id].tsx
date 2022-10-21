753648b4a918687f879224642298761a92bf4182
paurock <tselenkor@gmail.com>
2022-10-21 08:34:44 +0300

    get back destination folder

---
+++ pages/destinations/[id].tsx
@@ -0,0 +1,6 @@
+import { NextPage } from 'next'
+import { DestinationPage } from 'pages/DestinationPage'
+
+const DestinationId: NextPage = () => <DestinationPage />
+
+export default DestinationId
---
+++ pages/destinations/index.tsx
@@ -0,0 +1,6 @@
+import { NextPage } from 'next'
+import { DestinationPage } from 'pages/DestinationPage'
+
+const Destination: NextPage = () => <DestinationPage />
+
+export default Destination
