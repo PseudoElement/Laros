1f5dbc398cf28dd480f28f3977cc4ab22881fa6e
paurock <tselenkor@gmail.com>
2022-10-21 09:23:24 +0300

    fix errors for build

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,3 @@
-
 import { Input } from 'components/Input'
 import { Modal } from 'components/Modal'
 import { Radio } from 'components/Radio'
@@ -6,7 +5,7 @@ import { Slider } from 'features'
 import { ContactForm } from 'features/ContactForm'
 import { CategoryCard } from 'pages/TravelPlannerPage/CategoryCard'
 import { FC, useState } from 'react'
-import {Checkbox} from "../../components/Checkbox";
+import { Checkbox } from '../../components/Checkbox'
 import { moreCategoriesMock } from 'shared/mocks/tripPlanner'
 import { Tags } from 'features/Tags/Tags'
 import { mockTags } from 'shared/mocks/tags'
@@ -16,12 +15,13 @@ export const HomePage: FC = () => {
   const [input, setInput] = useState<any>('')
   const [checkboxValue, setCheckboxValue] = useState('man')
 
-
-
   const [tags, setTags] = useState(mockTags)
   //TODO move to example page
-  return <div style={{ marginTop: '15px', backgroundColor: '#FAFBFC', width: '100%' }}>
-    <div className='row'>
+  return (
+    <div
+      style={{ marginTop: '15px', backgroundColor: '#FAFBFC', width: '100%' }}
+    >
+      <div className='row'>
         <Input
           label='Earliest depature'
           type='date'
@@ -30,36 +30,59 @@ export const HomePage: FC = () => {
         />
       </div>
 
-      <div style={{ marginTop: '15px' }} onClick={() => setIsModalOpen(true)} ></div>
-
-
-
+      <div
+        style={{ marginTop: '15px' }}
+        onClick={() => setIsModalOpen(true)}
+      ></div>
 
       <div>
         <Slider>
           {moreCategoriesMock.map((card, id) => {
-            return (
-              <CategoryCard {...card} key={id} vertical />
-            )
+            return <CategoryCard {...card} key={id} vertical />
           })}
         </Slider>
       </div>
       <Slider>
         {moreCategoriesMock.map((card, id) => {
-          return (
-            <CategoryCard {...card} key={id} />
-          )
+          return <CategoryCard {...card} key={id} />
         })}
       </Slider>
 
-
-      //TODO move to example page
-    <ContactForm />
-    <Radio onChange={(v) => setCheckboxValue(v)} name='sex' options={[{ label: 'Man', value: 'man' }, { label: 'Woman', value: 'woman' }, { label: 'No', value: 'no' }]} value={checkboxValue} />
-    <Input id='num' value='' type='phone' label='Number' onChange={(v) => setInput(v)} />
-    <Input placeholder='Mark' id='name' value={input} label='Fullname and surname' onChange={(v) => setInput(v)} />
-    <Modal title='Contact Form' isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>Hello!</Modal>
-    <Checkbox label={"Текст Чекбокса"}/>
-  <Tags tags={tags} onChange={setTags} />
+      {/*//TODO move to example page*/}
+      <ContactForm />
+      <Radio
+        onChange={v => setCheckboxValue(v)}
+        name='sex'
+        options={[
+          { label: 'Man', value: 'man' },
+          { label: 'Woman', value: 'woman' },
+          { label: 'No', value: 'no' },
+        ]}
+        value={checkboxValue}
+      />
+      <Input
+        id='num'
+        value=''
+        type='phone'
+        label='Number'
+        onChange={v => setInput(v)}
+      />
+      <Input
+        placeholder='Mark'
+        id='name'
+        value={input}
+        label='Fullname and surname'
+        onChange={v => setInput(v)}
+      />
+      <Modal
+        title='Contact Form'
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      >
+        Hello!
+      </Modal>
+      <Checkbox label={'Текст Чекбокса'} />
+      <Tags tags={tags} onChange={setTags} />
     </div>
+  )
 }
