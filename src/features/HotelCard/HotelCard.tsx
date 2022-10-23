28d7e310c7e4d5f05b3b41247207c0183b16a40c
Alim Budaev <87270192+loveroftheRussianQueen@users.noreply.github.com>
2022-10-23 14:31:20 +0300

    first draft (#17)
    * first draft
    
    * fixed the button
    
    * added props
    
    * changed the color
    
    * added image
    
    * revert commit
    
    * creating new branch
    
    * added tags
    
    * removed the bug
    
    * moved home to example page
    
    * moved example page
    
    * added tags props
    
    Co-authored-by: Your Name <al1m.budaev@yandex.ru>

---
+++ src/features/HotelCard/HotelCard.tsx
@@ -0,0 +1,73 @@
+import React, { FC, useState } from 'react'
+import ReactStars from 'react-rating-stars-component'
+import { Tag } from 'shared/types/tag'
+import Image from 'next/image'
+import cn from 'classnames'
+
+import s from './HotelCard.module.scss'
+
+import hotel_pic from '../../../public/assets/images/area-images/cart__hotel-four.jpg'
+import { mockTags } from 'shared/mocks/tags'
+
+interface HotelCardProps {
+  rating: number
+  type: string
+  name: string
+  fromPrice: number
+  period: string
+  tags: Tag[]
+  onClick?: (id: number) => void
+}
+
+export const HotelCard: FC<HotelCardProps> = ({
+  rating,
+  type,
+  name,
+  fromPrice,
+  period,
+  tags,
+  onClick,
+}) => {
+  return (
+    <div className={s.container}>
+      <div className={s.slider}>
+        <Image
+          src={hotel_pic}
+          objectFit='cover'
+          height='850'
+          alt='Hotel Picture'
+        />
+      </div>
+      <div className={s.header}>
+        <div className={s.rating}>
+          <ReactStars
+            count={5}
+            value={rating}
+            size={24}
+            activeColor='#ffd700'
+            edit={false}
+          />
+        </div>
+        <div className={s.type}>{type}</div>
+        <div className={s.name}>{name}</div>
+      </div>
+      <div className={s.info}>
+        <div className={s.block}>
+          <p className={s.text}>From</p>
+          <p className={s.price}>{fromPrice} CHF/Night</p>
+          <p className={s.text}>Pro person</p>
+        </div>
+        <div className={s.block}>
+          <p className={s.text}>Travel Period</p>
+          <p className={s.period}>{period}</p>
+        </div>
+      </div>
+      <div className={s.tags}>
+        {tags.map(tag => (
+          <p className={s.tag}>{tag.name}</p>
+        ))}
+      </div>
+      <button className={s.button}>Learn more</button>
+    </div>
+  )
+}
---
+++ src/pages/ExamplePage/ExamplePage.tsx
@@ -0,0 +1,112 @@
+import React, { FC, useState } from 'react'
+import { HotelCard } from 'features/HotelCard'
+import { Input } from 'components/Input'
+import { Slider } from 'features'
+import { CategoryCard } from 'pages/TravelPlannerPage/CategoryCard'
+import { moreCategoriesMock } from 'shared/mocks/tripPlanner'
+import { ContactForm } from 'features/ContactForm'
+import { Radio } from 'components/Radio'
+import { Modal } from 'components'
+import { Checkbox } from 'components/Checkbox'
+import { Tags } from 'components/Tags'
+import { mockTags } from 'shared/mocks/tags'
+import { ChangeLocationModal } from 'features/ChangeLocationModal'
+import { destinationsMock } from 'shared/mocks/destinations'
+
+export const ExamplePage: FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [input, setInput] = useState<any>('')
+  const [checkboxValue, setCheckboxValue] = useState('man')
+  const [counter, setCounter] = useState(3)
+  const [tags, setTags] = useState(mockTags)
+  return (
+    <div
+      style={{
+        marginTop: '15px',
+        width: '100%',
+        backgroundColor: '#FAFBFC',
+      }}
+    >
+      <div
+        style={{ marginTop: '15px', backgroundColor: '#FAFBFC', width: '100%' }}
+      >
+        <div className='row'>
+          <Input
+            label='Earliest depature'
+            type='date'
+            id='100'
+            onChange={e => console.log('get date')}
+          />
+        </div>
+        <Input
+          label='Counter'
+          type='number'
+          id='100'
+          onChange={value => setCounter(value)}
+          value={counter}
+          withCounter
+          max={5}
+          min={1}
+        />
+        <div
+          style={{ marginTop: '15px' }}
+          onClick={() => setIsModalOpen(true)}
+        ></div>
+        <div>
+          <Slider>
+            {moreCategoriesMock.map((card, id) => {
+              return <CategoryCard {...card} key={id} vertical />
+            })}
+          </Slider>
+        </div>
+        <Slider>
+          {moreCategoriesMock.map((card, id) => {
+            return <CategoryCard {...card} key={id} />
+          })}
+        </Slider>
+        //TODO move to example page
+        <ContactForm />
+        <Radio
+          onChange={v => setCheckboxValue(v)}
+          name='sex'
+          options={[
+            { label: 'Man', value: 'man' },
+            { label: 'Woman', value: 'woman' },
+            { label: 'No', value: 'no' },
+          ]}
+          value={checkboxValue}
+        />
+        <Input
+          id='num'
+          value=''
+          type='phone'
+          label='Number'
+          onChange={v => setInput(v)}
+        />
+        <Input
+          placeholder='Mark'
+          id='name'
+          value={input}
+          label='Fullname and surname'
+          onChange={v => setInput(v)}
+        />
+        <Modal
+          title='Contact Form'
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+        >
+          Hello!
+        </Modal>
+        <Checkbox label={'Текст Чекбокса'} />
+        <Tags tags={tags} onChange={setTags} />
+        <ChangeLocationModal
+          destinations={destinationsMock}
+          onClick={() => 1}
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+        />
+      </div>
+      <HotelCard tags={tags} />
+    </div>
+  )
+}
