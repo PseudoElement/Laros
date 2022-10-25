b3eeac7695f1f5b4be4f5949337c0518cb2f3703
nixjke <aziz.10.255@gmail.com>
2022-10-25 19:52:18 +0300

    add fullscreen

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -3,41 +3,46 @@ import React from 'react'
 import s from './Map.module.scss'
 
 import 'leaflet/dist/leaflet.css'
+import 'leaflet-fullscreen/dist/Leaflet.fullscreen.js'
+import 'leaflet-fullscreen/dist/leaflet.fullscreen.css'
 
-import { MapContainer, Marker, Popup, TileLayer, useMap, ZoomControl } from 'react-leaflet'
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  ZoomControl,
+} from 'react-leaflet'
 import { Icon } from 'leaflet'
 
 import mapMarker from '../../../public/assets/images/map-marker.svg'
-import Image from 'next/image'
 
 const myIcon = new Icon({
   iconUrl: mapMarker.src,
-  iconSize: [25, 25]
-});
+  iconSize: [25, 25],
+})
 
 const Map = () => {
   return (
-    <>
-      <MapContainer
-        className={s.map}
-        center={[51.505, -0.09]}
-        zoom={13}
-        scrollWheelZoom={false}
-        zoomControl={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        />
-        <Marker icon={myIcon} position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-        <ZoomControl position='topright' />
-      </MapContainer>
-      <Image src={mapMarker} />
-    </>
+    <MapContainer
+      className={s.map}
+      center={[51.505, -0.09]}
+      fullscreenControl={true}
+      zoom={13}
+      scrollWheelZoom={false}
+      zoomControl={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+      <Marker icon={myIcon} position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+      <ZoomControl position='topright' />
+    </MapContainer>
   )
 }
 
