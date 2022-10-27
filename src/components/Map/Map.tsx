b23a9c1530f871787de08c61add1057387d2755e
nixjke <aziz.10.255@gmail.com>
2022-10-27 16:48:14 +0300

    add gjson

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import L, { Icon, latLng } from 'leaflet'
 
 import s from './Map.module.scss'
 
@@ -12,23 +13,43 @@ import {
   Popup,
   TileLayer,
   ZoomControl,
+  GeoJSON,
+  FeatureGroup,
 } from 'react-leaflet'
-import { Icon } from 'leaflet'
+import 'leaflet/dist/images/marker-shadow.png'
 
 import mapMarker from '../../../public/assets/images/map-marker.svg'
-
-const myIcon = new Icon({
-  iconUrl: mapMarker.src,
-  iconSize: [25, 25],
-})
+import { gjson } from './geojson'
+import { features } from 'process'
 
 const Map = () => {
+  const coordinates = gjson.features[0].geometry.coordinates
+
+  const center = ([coordinates[0], coordinates[1]] = [
+    coordinates[1],
+    coordinates[0],
+  ])
+
+  const icon = gjson.features.map(f => f.properties.iconUrl || '')
+
+  const myIcon = new Icon({
+    iconUrl: icon[0],
+    // iconUrl: mapMarker.src,
+    iconSize: [25, 25],
+  })
+
+  // L.Icon.Default.mergeOptions({
+  //   iconRetinaUrl: mapMarker.src,
+  //   iconUrl: mapMarker.src,
+  //   shadowUrl: null,
+  // })
+
   return (
     <MapContainer
       className={s.map}
-      center={[51.505, -0.09]}
       fullscreenControl={true}
-      zoom={13}
+      center={center as any}
+      zoom={11}
       scrollWheelZoom={false}
       zoomControl={false}
     >
@@ -36,11 +57,18 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      <Marker icon={myIcon} position={[51.505, -0.09]}>
+      {/* <Marker icon={myIcon} position={center as any}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
-      </Marker>
+      </Marker> */}
+      <FeatureGroup>
+        {gjson.features.map((features, i) => (
+          <>
+            <GeoJSON key={i} data={features as any} />
+          </>
+        ))}
+      </FeatureGroup>
       <ZoomControl position='topright' />
     </MapContainer>
   )
