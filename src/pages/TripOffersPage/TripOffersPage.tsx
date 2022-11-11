18f0308cfc31c823af5ff05222e5283c1862278c
Martin Li <martinhappy2017@icloud.com>
2022-11-11 09:41:43 +0300

    connect filters on trip page

--- src/pages/TripOffersPage/TripOffersPage.tsx
+++ src/pages/TripOffersPage/TripOffersPage.tsx
@@ -1,51 +1,137 @@
-import { useRouter } from 'next/router'
-import { TripCard } from 'pages/TravelPlannerPage/TripCard'
 import { FC, useEffect, useState } from 'react'
-import { tripPageInfo } from 'shared/mocks/tripInfo'
-import { tripListMock } from 'shared/mocks/tripList'
-import { TripItem } from 'shared/types/trip'
 import s from './TripOffersPage.module.scss'
 import cn from 'classnames'
-import bg from '/public/assets/images/tripListPageBg.png'
-import listIcon from '/public/assets/images/list.svg'
-import gridIcon from '/public/assets/images/grid.svg'
-import { useForm } from 'react-hook-form'
+
+import { useRouter } from 'next/router'
+import { Controller, useForm } from 'react-hook-form'
+
+import { tripPageInfo } from 'shared/mocks/tripInfo'
+
+import listIcon from '/public/assets/images/list.svg?url'
+import gridIcon from '/public/assets/images/grid.svg?url'
 import { Tags } from 'components/Tags'
 import Image from 'next/image'
 import { useGetTrips } from 'shared/hooks/useGetTrips'
-import { divide } from 'lodash'
+import { Select } from 'components/Select'
+import { useAppDispatch, useAppSelector } from 'shared/hooks/redux'
+import { getTripCategoriesThunk } from 'store/slices/trips/thunk'
+import { getDestinationsThunk } from 'store/slices/destinations/thunk'
+import { getRootDestinations, getSubRegions } from 'store/slices/destinations/selectors'
+import { getHotelTags } from 'shared/api/routes/hotels'
+import { Tag } from 'shared/types/tag'
+import { TripSortOptions } from 'shared/constants/filters'
+import { TripCategory, TripFilterParams } from 'shared/types/trip'
+import { Option } from 'shared/types'
+import { TripCard } from 'features'
 
 enum View {
   LIST,
   GRID,
 }
 export const TripOffersPage: FC = () => {
-  const { query } = useRouter()
-  const [tripCatInfo, setTripCatInfo] = useState<any>({})
+  const { query, push } = useRouter()
+  const { category } = query
+  const { control, watch } = useForm()
+  const dispatch = useAppDispatch();
+
+  const [params, setParams] = useState<Partial<TripFilterParams>>({})
+  const [trips, isLoading, handleReady] = useGetTrips(params)
+  const [tripCategoryInfo, setTripCatInfo] = useState<TripCategory | null>()
   const [view, setView] = useState(View.GRID)
-  const { handleSubmit, control } = useForm()
-  const [trips, isLoading, handleReady] = useGetTrips(query)
-  console.log('trips :', trips)
+  const [tags, setTags] = useState<Tag[]>([])
+  const [region, setRegion] = useState<Option | null>(null)
+
+  // const tripCategoryInfo = useAppSelector((state) => state.trips.categories.find((cat) => cat.id === Number(category)));
+  const destinations = useAppSelector((state) => state.destinations.destinations);
+  const regions = getRootDestinations(destinations)
+  const subregions = useAppSelector((state) => getSubRegions(state, region?.value ?? null));
+
+  const updateRequest = (form) => {
+    form.region && setRegion(form.region)
+    const subregions = form.subregions ? form.subregions.map((region: Option) => region.value) : [];
+    const destination = [subregions, region?.value ?? ''].join(', ')
+    setParams({ destination, ordering: form.ordering?.value, })
+  }
+
+  useEffect(() => {
+    const loadHotelTags = async () => {
+      try {
+        const { data } = await getHotelTags()
+        setTags(data.data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    loadHotelTags()
+    dispatch(getTripCategoriesThunk())
+    dispatch(getDestinationsThunk())
+  }, []);
+
   useEffect(() => handleReady(true), [query])
 
   useEffect(() => {
-    const { category } = query
+    const subscription = watch((value) => updateRequest(value));
+    return () => subscription.unsubscribe();
+  }, [category, watch]);
+
+  useEffect(() => {
     setTripCatInfo(tripPageInfo)
-  }, [query])
+  }, [category])
+
   return (
     <div className={s.container}>
       <div
         className={s.bg}
         style={{
-          backgroundImage: `url(${bg.src})`,
+          backgroundImage: `url(${tripCategoryInfo?.image})`,
         }}
       />
-      <div className={s.title}>{tripCatInfo?.title}</div>
-      <div className={s.description}>{tripCatInfo?.description}</div>
+      <div className={s.title}>{tripCategoryInfo?.name}</div>
+      <div className={s.description}>{tripCategoryInfo?.description}</div>
       <div className={s.filters}>
         <div className={s.filterTitle}>Sort by</div>
         <div className={s.filterSelects}>
-          <Tags tags={[]} onChange={() => 1} />
+          <Controller
+            name='region'
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <Select options={regions.map((region) => ({ label: region.name, value: region.id.toString() })) ?? []} onChange={onChange} value={value} />
+            )}
+          />
+          <Controller
+            name='subregions'
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <Select options={subregions.map((region) => ({ label: region.name, value: region.id.toString() })) ?? []} onChange={onChange} value={value} isMulti />
+            )}
+          />
+          <Controller
+            name='duration'
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <Select options={[]} onChange={onChange} value={value} />
+            )}
+          />
+          <div className={s.sortFrom}>From </div>
+          <Controller
+            name='ordering'
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <Select options={TripSortOptions} onChange={onChange} value={value} hasArrow={false} />
+            )}
+          />
+
+        </div>
+        <div className={s.tagSwitchFilters}>
+          <div className={s.tags}>
+            <Controller
+              name='tags'
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <Tags tags={tags} onChange={onChange} value={value} />
+              )}
+            />
+          </div>
           <div className={s.viewSwitch}>
             <span
               onClick={() => setView(View.LIST)}
@@ -53,7 +139,7 @@ export const TripOffersPage: FC = () => {
                 [s.viewSelected]: view === View.LIST,
               })}
             >
-              <Image src={listIcon} />
+              <Image src={listIcon} width={16} height={16} />
             </span>
             <span
               onClick={() => setView(View.GRID)}
@@ -61,7 +147,7 @@ export const TripOffersPage: FC = () => {
                 [s.viewSelected]: view === View.GRID,
               })}
             >
-              <Image src={gridIcon} />
+              <Image src={gridIcon} width={16} height={16} />
             </span>
           </div>
         </div>
@@ -74,7 +160,6 @@ export const TripOffersPage: FC = () => {
             return (
               // @ts-ignore
               <TripCard
-                period={'12'}
                 key={idx}
                 {...offer}
                 wide={view === View.LIST}
@@ -85,3 +170,4 @@ export const TripOffersPage: FC = () => {
     </div>
   )
 }
+
