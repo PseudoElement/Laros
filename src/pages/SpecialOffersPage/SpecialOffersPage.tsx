7ed0161b00fc92dde9100ca1c774faace2c1d716
Martin Li <martinhappy2017@icloud.com>
2022-11-12 12:53:20 +0300

    add container

--- src/pages/SpecialOffersPage/SpecialOffersPage.tsx
+++ src/pages/SpecialOffersPage/SpecialOffersPage.tsx
@@ -1,7 +1,18 @@
+import { Container } from 'components'
 import { FC } from 'react'
+import s from './SpecialOffersPage.module.scss';
 
 export const SpecialOffersPage: FC = () => {
     return (
-        <div></div>
+        <Container>
+            <div className={s.wrapper} key={s.title}>
+                <div className={s.title}>Special offers</div>
+                <div className={s.nav} key={s.title}>
+                    <div className={s.subtitle}>
+                        Faucibus enim sit leo, purus, odio erat. Neque scelerisque volutpat morbi proin. Massa quis montes, scelerisque commodo elit erat in urna id. Purus sit odio egestas venenatis viverra blandit amet vitae.
+                    </div>
+                </div>
+            </div>
+        </Container>
     )
 }
