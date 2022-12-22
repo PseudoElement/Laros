042509e546da50371d41656e6c1af3d2ad5201d7
Ilya Rogachev <88387520+ex1one@users.noreply.github.com>
2022-12-22 16:55:21 +0500

    Added logic for areas (#106)
    * add areas from api
    
    * fix error with button
    
    * change Macedonia map and some fixes

--- src/features/DestinationAreas/DestinationAreas.tsx
+++ src/features/DestinationAreas/DestinationAreas.tsx
@@ -1,35 +1,28 @@
 import { FC } from 'react'
 import Image, { StaticImageData } from 'next/image'
 
-import { mockAreas } from 'shared/mocks/areas'
-import { mockAreasType } from 'shared/types/areas'
-
-import { useTranslate } from 'shared/hooks/useTranslate'
-
 import s from './DestinationAreas.module.scss'
 
 export interface DestinationAreasProps {
   name: string
+  areas: string[] | StaticImageData[] | HTMLImageElement[]
 }
 
-const DestinationAreas: FC<DestinationAreasProps> = ({ name }) => {
-  const t = useTranslate()
-  const areas: StaticImageData[] = mockAreas[name as keyof mockAreasType]
-
+const DestinationAreas: FC<DestinationAreasProps> = ({ name, areas }) => {
   return (
     <div className={s.container}>
-      <h3 className={s.title}>
-        {t('destinationsSubRegion.areasOfTitle')} {name}
-      </h3>
+      <h3 className={s.title}>Areas of {name}</h3>
+
       <p className={s.description}>
-        {t('destinationsSubRegion.areasOfSubTitle')}
+        At ultrices rhoncus sit vel viverra viverra. Arcu pellentesque gravida
+        in orci, pretium nulla volutpat leo.
       </p>
 
       <div className={s.wrapperImages}>
         {areas.map((area, index) => (
           <div className={s.areasImage} key={index}>
             {/*TODO add a block with text and place it by positioning*/}
-            <Image key={area.src} src={area} layout={'fill'} alt='' />
+            <Image key={`${area}-${index}`} src={area} layout={'fill'} alt='' />
           </div>
         ))}
       </div>
