569a6f041e98d292cc03bf597c13b5a72e1212e1
Джахпарова Тамара <ragimova-tamara@mail.ru>
2023-02-01 13:36:06 +0300

    fix: pagination and filters in hotels

--- src/features/DestinationHotels/DestinationHotels.tsx
+++ src/features/DestinationHotels/DestinationHotels.tsx
@@ -19,7 +19,7 @@ interface DestinationHotelsProps {
 }
 
 export const DestinationHotels: FC<DestinationHotelsProps> = ({ map }) => {
-  const HOTEL_PAGINATION_PER_PAGE = 50
+  const HOTEL_PAGINATION_PER_PAGE = 10
 
   const { query } = useRouter()
 
@@ -27,12 +27,13 @@ export const DestinationHotels: FC<DestinationHotelsProps> = ({ map }) => {
 
   const [params, setParams] = useState<Partial<HotelFilterParams>>({
     destination: currentDestination?.toString() ?? undefined,
+    size: HOTEL_PAGINATION_PER_PAGE,
   })
   const [newHotels, isLoading, handleReady] = useGetHotels(params)
   const [hotels, setHotels] = useState<Hotel[]>([])
   const [page, setPage] = useState(1)
   const [showMoreButton, setShowMoreButton] = useState<boolean>(
-    newHotels.length === HOTEL_PAGINATION_PER_PAGE
+    newHotels.length === HOTEL_PAGINATION_PER_PAGE - 1
   )
   const t = useTranslate()
 
@@ -51,7 +52,7 @@ export const DestinationHotels: FC<DestinationHotelsProps> = ({ map }) => {
   }, [params])
 
   useEffect(() => {
-    setShowMoreButton(newHotels.length === HOTEL_PAGINATION_PER_PAGE)
+    setShowMoreButton(newHotels.length === HOTEL_PAGINATION_PER_PAGE - 1)
 
     setHotels(prevState => {
       if (params.page === 1) return newHotels
@@ -69,7 +70,12 @@ export const DestinationHotels: FC<DestinationHotelsProps> = ({ map }) => {
   return (
     <div className={s.container}>
       <h3 className={s.title}>{t('hotels.sortTitle')}</h3>
-      <Sorting map={map} setParams={setParams} params={params} />
+      <Sorting
+        map={map}
+        setParams={setParams}
+        params={params}
+        setPage={setPage}
+      />
 
       {!isLoading && !hotels.length && (
         <div className={s.loading}>{t('common.emptyText')}</div>
