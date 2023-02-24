544b23b48d76e6b4266d2634446ecbcc1e56d2bd
Toma <ragimova-tamara@mail.ru>
2023-02-24 21:07:35 +0300

    feat: add proposals

--- src/shared/mocks/staticTravel.ts
+++ src/shared/mocks/staticTravel.ts
@@ -56,18 +56,36 @@ export const StaticTravelMock: StaticTravelItem[] = [
       ],
       video: '',
     },
-    highlighted: {
-      title: 'Luxusvorschläge und exotische Reisen',
-      text:
-        'Wenn Sie Luxusurlaub lieben, können wir Ihnen einmalige Resorts auf den Malediven, den Seychellen oder in der Südsee empfehlen und buchen.\n' +
-        'Natürlich sollten Sie dabei auch Mykonos und Santorini nicht vergessen, da diese Inseln in Griechenland für Luxusurlaub bekannt sind! \n',
-      link: '/#',
-      images: [
-        '/assets/images/static_travel_plannerPage/slider_6/slide_8.png',
-        '/assets/images/static_travel_plannerPage/slider_6/slide_9.png',
-        '/assets/images/static_travel_plannerPage/slider_6/slide_10.png',
-      ],
-    },
+    highlighted: [
+      {
+        title: 'Luxusvorschläge und exotische Reisen',
+        text:
+          'Wenn Sie Luxusurlaub lieben, können wir Ihnen einmalige Resorts auf den Malediven, den Seychellen oder in der Südsee empfehlen und buchen.\n' +
+          'Natürlich sollten Sie dabei auch Mykonos und Santorini nicht vergessen, da diese Inseln in Griechenland für Luxusurlaub bekannt sind! \n',
+        link: '/#',
+        images: [
+          '/assets/images/static_travel_plannerPage/slider_6/slide_8.png',
+          '/assets/images/static_travel_plannerPage/slider_6/slide_9.png',
+          '/assets/images/static_travel_plannerPage/slider_6/slide_10.png',
+        ],
+      },
+
+      {
+        title: 'Reisen zu zweit',
+        text:
+          'Lassen Sie uns gemeinsam die perfekte Reise für Sie und Ihre Reisebegleitung planen.  \n' +
+          'Mit unserem Wissen sorgen wir für eindrucksvolle Reiseerlebnisse und übernehmen die Organisation bis in jedes Detail, damit Sie Ihren Urlaub vor Ort rundum und sorgenfrei geniessen können.  \n',
+        link: '/#',
+        images: ['/assets/images/static_travel_plannerPage/gray.png'],
+      },
+
+      {
+        title: 'Dschungelsafari',
+        text: 'Sind Sie fasziniert von der Wildnis und Sonne Afrikas? Möchten Sie einmal Tiger, Leoparden und Elefanten in ihrem natürlichen Lebensraum hautnah erleben? Dann ist eine Safari das ideale Angebot für Sie! Kenia, Südafrika und Namibia sind nur einige der Möglichkeiten für diesen Reisewunsch. Unsere persönliche Erfahrung wird Ihre Reise zu einem ganz besonderen Erlebnis machen.\n',
+        link: '/#',
+        images: ['/assets/images/static_travel_plannerPage/gray.png'],
+      },
+    ],
   },
   {
     id: 7,
@@ -141,17 +159,37 @@ export const StaticTravelMock: StaticTravelItem[] = [
       ],
       video: '',
     },
-    highlighted: {
-      title: 'Reisen zu zweit',
-      text: 'Lassen Sie uns gemeinsam die perfekte Reise für Sie und Ihre Reisebegleitung planen. Mit unserem Wissen sorgen wir für eindrucksvolle Reiseerlebnisse und übernehmen die Organisation bis in jedes Detail, damit Sie Ihren Urlaub vor Ort rundum und sorgenfrei geniessen können.',
-      link: '/#',
-      images: [
-        '/assets/images/static_travel_plannerPage/slider_6/slide_11.png',
-        '/assets/images/static_travel_plannerPage/slider_6/slide_20.png',
-        '/assets/images/static_travel_plannerPage/slider_6/slide_13.png',
-        '/assets/images/static_travel_plannerPage/slider_6/slide_15.png',
-      ],
-    },
+    highlighted: [
+      {
+        title: 'Geschäftsreisen',
+        text: 'Ein engagierter Kundenbetreuer steht Ihnen zur Verfügung, um Ihre Geschäftsreisen zu organisieren. Durch die Bereitstellung einer 24/7-Notrufnummer stellen wir sicher, dass alle auftretenden Probleme gelöst werden. Wir können Ihrem Unternehmen helfen, seine Reiserichtlinien mit detaillierten Vorschlägen zu verbessern, wobei wir immer Ihre bestmöglichen finanziellen Interessen im Auge behalten.',
+        link: '/#',
+        images: [
+          '/assets/images/static_travel_plannerPage/slider_6/slide_11.png',
+          '/assets/images/static_travel_plannerPage/slider_6/slide_20.png',
+          '/assets/images/static_travel_plannerPage/slider_6/slide_13.png',
+          '/assets/images/static_travel_plannerPage/slider_6/slide_15.png',
+        ],
+      },
+
+      {
+        title:
+          'Online-Unternehmensreiseplattform - Synopsis Trip von Antaeus Travel Group',
+        text: 'Wir haben bereits eine digitale Reisebuchungsplattform für Unternehmen, Synopsis Trip, entwickelt, auf der Ihr Buchungsmanager jeden Reisenden schnell, sofort und kostengünstig online auf seinem Laptop, Handy oder Tablet buchen kann und sofortige Empfehlungen vom Reisebüro auf der anderen Seite seines Bildschirms erhalten kann! Die direkte Kommunikation mit unserem Corporate-Reiseberater über den Online Chat, die Schnittstelle mit Ihrer HR-Software über die API und die Ausgabe von Berichten, Statistiken und Spesen für jeden Reisenden machen die Plattform zur besten Lösung für Ihr Unternehmen.',
+        link: '/#',
+        images: ['/assets/images/static_travel_plannerPage/gray.png'],
+      },
+
+      {
+        title:
+          'M.I.C.E. in Griechenland & der Welt | Hybride oder vollständig virtuelle Veranstaltungen',
+        text:
+          'Das Organisieren von Firmenkonferenzen und -veranstaltungen ist für viele eine sehr aufwändige Arbeitsaufgabe. Wenn Sie Ihre nächste Jahrestagung in Athen oder einer anderen Region in Griechenland planen, versprechen wir Ihnen, dass Sie Ihre Meinung über die Konferenzorganisation ändern werden. Wir bieten Ihnen eine umfassende Palette von Dienstleistungen für die Organisation von Veranstaltungen, von der Suche nach einem Raum für die gewünschte Anzahl von Personen bis hin zu Unterkunft, Transport und Vorschlägen für die Freizeitgestaltung der Reisegruppe. Fragen Sie uns nach Ideen für einzigartige Events und Incentive-Reisen für Ihre Mitarbeiter! \n' +
+          'Der hohe Standard der Leistungserbringung ermöglicht es uns, Sie bei jeder Veranstaltung, die Sie komplett virtuell oder hybrid organisieren möchten, mit entsprechender Ausstattung an jedem Ort der Welt zu unterstützen.  \n',
+        link: '/#',
+        images: ['/assets/images/static_travel_plannerPage/gray.png'],
+      },
+    ],
   },
   {
     id: 10,
@@ -203,17 +241,33 @@ export const StaticTravelMock: StaticTravelItem[] = [
       ],
       video: '',
     },
-    highlighted: {
-      title: 'Dschungelsafari',
-      text: 'Sind Sie fasziniert von der Wildnis und Sonne Afrikas? Möchten Sie einmal Tiger, Leoparden und Elefanten in ihrem natürlichen Lebensraum hautnah erleben? Dann ist eine Safari das ideale Angebot für Sie! Kenia, Südafrika und Namibia sind nur einige der Möglichkeiten für diesen Reisewunsch. Unsere persönliche Erfahrung wird Ihre Reise zu einem ganz besonderen Erlebnis machen',
-      link: '/#',
-      images: [
-        '/assets/images/static_travel_plannerPage/slider_6/slide_4.png',
-        '/assets/images/static_travel_plannerPage/slider_6/slide_5.png',
-        '/assets/images/static_travel_plannerPage/slider_6/slide_6.png',
-        '/assets/images/static_travel_plannerPage/slider_6/slide_7.png',
-      ],
-    },
+    highlighted: [
+      {
+        title: 'Kunst- und Kultu',
+        text: 'Gehen Sie auf Reisen und lernen Sie die Werke bekannter',
+        link: '/#',
+        images: [
+          '/assets/images/static_travel_plannerPage/slider_6/slide_4.png',
+          '/assets/images/static_travel_plannerPage/slider_6/slide_5.png',
+          '/assets/images/static_travel_plannerPage/slider_6/slide_6.png',
+          '/assets/images/static_travel_plannerPage/slider_6/slide_7.png',
+        ],
+      },
+
+      {
+        title: 'Gastronomie & Wein',
+        text: 'Feinschmecker sind unsere Lieblingskunden! Verraten Sie uns, in welche Küche Sie eintauchen möchten und wir empfehlen Ihnen Destinationen mit kulinarischen Höhepunkten.',
+        link: '/#',
+        images: ['/assets/images/static_travel_plannerPage/gray.png'],
+      },
+
+      {
+        title: 'Yoga & Wellness',
+        text: 'Entspannen Sie sich und geniessen Sie die pure Erholung',
+        link: '/#',
+        images: ['/assets/images/static_travel_plannerPage/gray.png'],
+      },
+    ],
   },
   {
     id: 12,
